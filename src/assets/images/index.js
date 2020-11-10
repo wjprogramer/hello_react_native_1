72490cd985febf65cff47274bc34ae6db02117cc
jay_wu <jay_wu@gss.com.tw>
2020-11-10 15:18:06 +0800

    -

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,1 +1,19 @@
+export const IMG_BURGER_BIG_MAC = require('./burgers/t-mcdonalds-Big-Mac.jpg');
+export const IMG_BURGER_DOUBLE_QUARTER = require('./burgers/t-mcdonalds-Double-Quarter-Pounder-with-Cheese.jpg');
+export const IMG_BURGER_QUARTER = require('./burgers/t-mcdonalds-Quarter-Pounder-with-Cheese.jpg');
+
+export const IMG_NOODLES_COVER = require('./noodles/cover.jpg');
+export const IMG_NOODLES_LIST = [
+  require(`./noodles/noodles_1.jpg`),
+  require(`./noodles/noodles_2.jpg`),
+  require(`./noodles/noodles_3.jpg`),
+  require(`./noodles/noodles_4.jpg`),
+  require(`./noodles/noodles_5.jpg`),
+  require(`./noodles/noodles_6.jpg`),
+  require(`./noodles/noodles_7.jpg`),
+  require(`./noodles/noodles_8.jpg`),
+  require(`./noodles/noodles_9.jpg`),
+  require(`./noodles/noodles_10.jpg`),
+]
+
 export const IMG_CHIHIRO_1 = require('./chihiro/chihiro001.jpg'); 
